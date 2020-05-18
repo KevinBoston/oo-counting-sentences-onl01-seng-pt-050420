e743acb6de3434dbf09072f0911db5d1c0187ca1
Kevin Boston <kbos22@gmail.com>
2020-05-18 14:45:52 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,10 @@ class String
 
   def count_sentences
     sent_arr = self.split("." || "!" || "?")
-    sent_arr.delete_if(size < 2)
+    sent_arr.map do |size|
+      if size.length < 2
+        size
+    #sent_arr.delete_if(size < 2)
     sent_arr.count 
   end
 end
